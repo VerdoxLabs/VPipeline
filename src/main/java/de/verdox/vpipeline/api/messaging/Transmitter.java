eac65256979fcd894192db9a90075c5bb8167194
derverdox <lukasjonsson@outlook.de>
2023-01-25 18:41:45 +0100

    Changed Transmitter to fit new messaging service impl

--- src/main/java/de/verdox/vpipeline/api/messaging/Transmitter.java
+++ src/main/java/de/verdox/vpipeline/api/messaging/Transmitter.java
@@ -1,6 +1,6 @@
 package de.verdox.vpipeline.api.messaging;
 
-import de.verdox.vpipeline.api.messaging.message.Message;
+import de.verdox.vpipeline.api.messaging.instruction.Instruction;
 import de.verdox.vpipeline.api.pipeline.core.SystemPart;
 
 import java.util.UUID;
@@ -11,9 +11,9 @@ import java.util.UUID;
  * @date 19.06.2022 12:25
  */
 public interface Transmitter extends SystemPart {
-    long sendMessage(Message message, UUID... receivers);
+    long sendMessage(Instruction<?> message, UUID... receivers);
 
-    long broadcastMessage(Message message);
+    long broadcastMessage(Instruction<?> message);
 
     void setMessagingService(MessagingService messagingService);
 
