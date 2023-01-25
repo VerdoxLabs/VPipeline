fd4e5dfd60e770fd90eae1438a30c39ec0d49aea
derverdox <lukasjonsson@outlook.de>
2023-01-25 18:37:30 +0100

    Changed MessageEventImpl to fit new messaging service impl

--- src/main/java/de/verdox/vpipeline/impl/messaging/event/MessageEventImpl.java
+++ src/main/java/de/verdox/vpipeline/impl/messaging/event/MessageEventImpl.java
@@ -1,21 +1,22 @@
 package de.verdox.vpipeline.impl.messaging.event;
 
 import de.verdox.vpipeline.api.messaging.event.MessageEvent;
-import de.verdox.vpipeline.api.messaging.message.Message;
+import de.verdox.vpipeline.api.messaging.instruction.Instruction;
+
 
 /**
  * @version 1.0
  * @Author: Lukas Jonsson (Verdox)
  * @date 19.06.2022 13:30
  */
-public record MessageEventImpl(String channelName, Message message) implements MessageEvent {
+public record MessageEventImpl(String channelName, Instruction<?> instruction) implements MessageEvent {
     @Override
     public String getChannelName() {
         return channelName;
     }
 
     @Override
-    public Message getMessage() {
-        return message;
+    public Instruction<?> getMessage() {
+        return instruction;
     }
 }
