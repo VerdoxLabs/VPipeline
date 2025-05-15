6158d3ec2d52afcce9feb498a7a2099e76d7a5d5
verdox <mail.ysp@web.de>
2025-05-15 11:22:59 +0000

    Pass exception to signature

--- src/main/java/de/verdox/vpipeline/api/config/PipelineConfig.java
+++ src/main/java/de/verdox/vpipeline/api/config/PipelineConfig.java
@@ -2,6 +2,7 @@ package de.verdox.vpipeline.api.config;
 
 import de.verdox.vpipeline.api.NetworkParticipant;
 import de.verdox.vpipeline.api.VNetwork;
+import de.verdox.vserializer.exception.SerializationException;
 import de.verdox.vserializer.generic.SerializationContext;
 import de.verdox.vserializer.generic.SerializationElement;
 import de.verdox.vserializer.json.JsonSerializerContext;
@@ -12,7 +13,7 @@ import java.io.IOException;
 public class PipelineConfig {
     private final File file;
 
-    public PipelineConfig(File file) throws IOException {
+    public PipelineConfig(File file) throws IOException, SerializationException {
         this(file, VNetwork.getConstructionService().createNetworkParticipant()
                 .withName("pipeline")
                 .withPipeline()
@@ -20,11 +21,11 @@ public class PipelineConfig {
                 .build());
     }
 
-    public PipelineConfig(File file, NetworkParticipant defaultValue) throws IOException {
+    public PipelineConfig(File file, NetworkParticipant defaultValue) throws IOException, SerializationException {
         this(file, defaultValue, false);
     }
 
-    public PipelineConfig(File file, NetworkParticipant defaultValue, boolean overwrite) throws IOException {
+    public PipelineConfig(File file, NetworkParticipant defaultValue, boolean overwrite) throws IOException, SerializationException {
         this.file = file;
 
         if(file.getParentFile() != null) {
@@ -41,7 +42,7 @@ public class PipelineConfig {
         }
     }
 
-    public NetworkParticipant load() throws IOException {
+    public NetworkParticipant load() throws IOException, SerializationException {
         SerializationElement element = new JsonSerializerContext().readFromFile(file);
         return NetworkParticipant.SERIALIZER.deserialize(element);
     }
